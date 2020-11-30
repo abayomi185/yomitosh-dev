16d426836234e2570911a5a18aebee9f291fb81f
Yomi Ikuru <abayomi.ikuru@gmail.com>
2020-11-30 19:36:46 +0000

    Add favicon

--- pages/index.js
+++ pages/index.js
@@ -27,6 +27,7 @@ function IndexPage() {
         content="John Doe"
         />
         <meta name="keywords" content="Yomitosh, Yomi, PiServer, PiDeck, Photo, YouTube, Hub, CV"/>
+        <link rel="icon" href="/favicon.ico" />
         <link href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossOrigin="anonymous"></link>
       </Head>
 
