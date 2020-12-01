1bd3d7f4e19d9df459e0fc5ac939f0725f56a092
Yomi Ikuru <abayomi.ikuru@gmail.com>
2020-12-01 00:39:00 +0000

    update analytics

--- utils/analytics.js
+++ utils/analytics.js
@@ -2,7 +2,7 @@ import ReactGA from 'react-ga'
 
 export const initGA = () => {
   console.log('GA init')
-  ReactGA.initialize('G-E0G9YE033C')
+  ReactGA.initialize('UA-131516762-5')
 }
 
 export const logPageView = () => {
