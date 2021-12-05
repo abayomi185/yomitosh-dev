f9f5d4a687eebf7ff64e3baf8e65c6f7b64a3e64
Yomi Ikuru <captyomjnr@gmail.com>
2021-12-05 15:18:08 +0000

    Update Header.js

--- components/Header.js
+++ components/Header.js
@@ -3,7 +3,7 @@ function Header() {
     <header className="container flex text-center mx-auto py-10">
       <div className="md:w-4/6 mx-auto">
         <img src="/yomi.jpg" className="h-24 rounded-full mx-auto mb-5" />
-        <h1 className="font-bold">Abayomi Ikuru</h1>
+        <h1 className="font-bold">Abayomi O-Ikuru</h1>
         <p>Electronic Engineer with a liking for <br/> Software Engineering & DevOps.</p>
       </div>
     </header>
