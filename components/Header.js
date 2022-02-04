855719ae45cfbd7cdd27e4e1f217b81f3eb340e7
Yomi Ikuru <abayomi.ikuru@gmail.com>
2022-02-04 15:52:08 +0000

    Update Header

--- components/Header.js
+++ components/Header.js
@@ -4,11 +4,15 @@ function Header() {
       <div className="md:w-4/6 mx-auto">
         <img src="/yomi.jpg" className="h-24 rounded-full mx-auto mb-5" />
         <h1 className="font-bold">Abayomi O-Ikuru</h1>
-        <p>Electronic Engineer with a liking for <br/> Software Engineering & DevOps.</p>
+        <p>
+          Artificial Intelligence MSc Student
+          <br />
+          with Electronic Engineering background.
+          <br /> Likes Software Dev. and DevOps.
+        </p>
       </div>
     </header>
   );
 }
 
 export default Header;
-
