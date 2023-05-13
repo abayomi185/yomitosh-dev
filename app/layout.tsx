9d58ea39260f008964d034dbffeead52db15c6d9
Yomi Ikuru <captyomjnr@gmail.com>
2023-05-13 21:30:05 +0100

    Moving to app directory

---
+++ app/layout.tsx
@@ -0,0 +1,17 @@
+// Data in the head tags
+export const metadata = {
+  title: "",
+  description: "",
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body>{children}</body>
+    </html>
+  );
+}
