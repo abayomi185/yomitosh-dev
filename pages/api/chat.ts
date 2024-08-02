cd6fb3345d7df73350c83a1705611bb0fa07d8a5
Yomi Ikuru <captyomjnr@gmail.com>
2024-08-02 12:39:02 +0100

    feat: move constant prompt to a constants directory

--- pages/api/chat.ts
+++ pages/api/chat.ts
@@ -5,7 +5,7 @@ export const config = {
 import { createParser } from "eventsource-parser";
 import type { NextFetchEvent, NextRequest } from "next/server";
 import { GPTModel, OpenAIMessage, OpenAIMessageContent } from "@type/chat";
-import { cv_prompt } from "../prompt";
+import { cv_prompt } from "../../constants/prompt";
 
 const OPENAI_COMPLETIONS_ENDPOINT =
   "https://api.openai.com/v1/chat/completions";
